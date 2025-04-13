b9a55973ff4c5df06f3a91f859ad69f9d6e1e0d0
RePuLs05 <vaja.zoidze.05@gmail.com>
2025-04-13 20:59:47 +0400

    feat: update sort logic and add function to sort input values

--- js-Number-Sorter/script.js
+++ js-Number-Sorter/script.js
@@ -2,8 +2,45 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
   event.preventDefault();
-  const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
-  
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = selectionSort(inputValues);
+
+  updateUI(sortedValues);
+}
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+
+    }
+  }
 }
 
 sortButton.addEventListener("click", sortInputArray);
